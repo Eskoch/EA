450ba45049bd6c26020a772dc3d0114babc7f103
Eskoch <abebeeskender@gmial.com>
2021-08-04 22:49:38 -0500

    assignment16

---
+++ assignment16/SpringBoot/server1/src/test/java/edu/miu/cs/cs544/Server1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs544;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Server1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
